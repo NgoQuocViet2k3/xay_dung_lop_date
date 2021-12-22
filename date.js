8e2d0249df9fc3dd126129a93d26ca286e812800
ruukichi <ngovietlc93@gmail.com>
2021-12-22 11:52:52 +0700

    xay dung lop date

---
+++ date.js
@@ -0,0 +1,36 @@
+class MyDate{
+    day;
+    month;
+    year;
+constructor(day,month,year) {
+    this.day=day;
+    this.month=month;
+    this.year=year;
+}
+ getDay (){
+    return this.day;
+ }
+ setDay (day){
+    this.day= day
+ }
+ getMonth (){
+    return this.month;
+ }
+ setMonth (month){
+     this.month=month;
+ }
+ getYear (){
+    return this.year;
+ }
+ setYear (year){
+    this.year=year;
+ }
+ setDate (day,month,year){
+    this.day=day;
+    this.month=month;
+    this.year=year;
+ }
+toString(){
+    alert(this.day+"/"+this.month+"/"+this.year);
+}
+}
